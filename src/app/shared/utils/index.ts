a6b9f0128db9cf01b8665ab4bd7106c9b8cba8c8
ZeevKatz <zeevk6@gmail.com>
2019-09-12 23:21:10 +0300

    add throttle function to the shared utilities

--- src/app/shared/utils/index.ts
+++ src/app/shared/utils/index.ts
@@ -1,2 +1,3 @@
 export { animate, Easing } from './animate.util';
+export { throttle } from './throttle.util';
 export { TinyGesture, TinyGestureOptions } from './tiny-gesture.util';
---
+++ src/app/shared/utils/throttle.util.ts
@@ -0,0 +1,12 @@
+export function throttle(func: () => void, limit: number) {
+  let inThrottle: any;
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  }
+};
